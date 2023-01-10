45edb81cbbebb3dcb5cb45e0ff3b6ed587fc2fd1
Isabella Costabile <costabileisa@gmail.com>
2023-01-10 13:06:46 -0500

    commit tree 1546049eaf30261757eb5fc7444f3099caf02a28

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,29 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onChangeType(type) {
+    setFilters({ type: type })
+  }
+
+  function onFindPetsClick() {
+    let fetchFilter;
+    if (filters.type === "all") {
+        fetchFilter = "";
+    } else {
+        fetchFilter = `?type=${filters.type}`
+    }
+    fetch(`http://localhost:3001/pets${fetchFilter}`)
+    .then(res => res.json())
+    .then(data => setPets(data))
+  }
+
+  function onAdoptPet(id) {
+    const updatedPets = pets.map(pet => {
+      return pet.id === id ? { ...pet, isAdopted: true } : pet;
+    });
+    setPets(updatedPets);
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +38,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser onAdoptPet={onAdoptPet} pets={pets} />
           </div>
         </div>
       </div>
